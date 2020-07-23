2e2f636731e4fc7d8040093de6c48608d66f510a
Kevin Solorio <kevin.solorio@hey.com>
2020-07-22 23:31:54 -0500

    updated gitignore and removed build dir

--- cmake-build-debug/vendor/googletest/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/ksolo/CLionProjects/cppnd-raytracer/vendor/googletest/googletest
-# Build directory: /Users/ksolo/CLionProjects/cppnd-raytracer/cmake-build-debug/vendor/googletest/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
