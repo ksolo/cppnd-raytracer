cddb7774bbe55503868e6fbeeee3fa1261ff2155
Kevin Solorio <kevin.solorio@hey.com>
2020-08-18 19:53:22 -0500

    wip: test on file output

--- tests/drawing/ppm_writer_test.cpp
+++ tests/drawing/ppm_writer_test.cpp
@@ -1,5 +1,6 @@
 #include "gtest/gtest.h"
 
+#include <filesystem>
 #include <fstream>
 #include <memory>
 
@@ -12,7 +13,20 @@ TEST(PPM_WRITER_TEST, test_initialization)
     std::string output_file("outfile.ppm");
     raytracer::ppm_writer writer(output_file, std::move(canvas));
 
-    std::fstream out("outfile.ppm");
+    std::fstream out(output_file);
     EXPECT_TRUE(out);
+
+    std::filesystem::path path(output_file);
+    std::filesystem::remove(path);
+}
+
+TEST(PPM_WRITER_TEST, test_header_output)
+{
+    std::unique_ptr<raytracer::canvas> canvas = std::make_unique<raytracer::canvas>(10, 20);
+    std::string output_file("outfile.ppm");
+    raytracer::ppm_writer writer(output_file, std::move(canvas));
+
+    std::filesystem::path path(output_file);
+    std::filesystem::remove(path);
 }
 
