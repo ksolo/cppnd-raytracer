f8dab41e57acd8a9f4cdea60ee07fe2160825afe
Kevin Solorio <kevin.solorio@hey.com>
2020-09-16 21:38:48 -0500

    updated initialization test

--- tests/data_structures/matrix_test.cpp
+++ tests/data_structures/matrix_test.cpp
@@ -13,4 +13,10 @@ TEST(MATRIX_TEST, initializing_matrix_with_data)
 
     raytracer::matrix m(data);
     EXPECT_EQ(m.at(0, 0), 1) << "expect value at 0,0 to be 1";
+    EXPECT_EQ(m.at(0, 3), 4) << "expect value at 0,3 to be 4";
+    EXPECT_EQ(m.at(1, 0), 5.5) << "expect value at 1,0 to be 5.5";
+    EXPECT_EQ(m.at(1, 2), 7.5) << "expect value at 1,2 to be 7.5";
+    EXPECT_EQ(m.at(2, 2), 11) << "expect value at 2,2 to be 11";
+    EXPECT_EQ(m.at(3, 0), 13.5) << "expect value at 3,0 to be 13.5";
+    EXPECT_EQ(m.at(3, 2), 15.5) << "expect value at 3,2 to be 15.5";
 }
